ff3f92aced92a5a71b865a1fffa4b453ef830f9f
caimiao0714 <miao.cai@slu.edu>
2019-07-08 11:02:53 -0500

    add zero-length issue reproducible example

--- functions/NHPP_functions.R
+++ functions/NHPP_functions.R
@@ -109,3 +109,78 @@ plot_events = function(event_dat, start_end_dat, cross_size = 2){
     theme_classic()
   return(p)
 }
+
+
+
+plptau = '
+functions{
+  real nhpp_log(vector t, real beta, real theta, real tau){
+    vector[num_elements(t)] loglik_part;
+    real loglikelihood;
+    for (i in 1:num_elements(t)){
+      loglik_part[i] = log(beta) - beta*log(theta) + (beta - 1)*log(t[i]);
+    }
+    loglikelihood = sum(loglik_part) - (tau/theta)^beta;
+    return loglikelihood;
+  }
+}
+data {
+  int<lower=0> N; //total # of obs
+  int<lower=0> K; //total # of groups
+  vector<lower=0>[K] tau;//truncated time
+  vector<lower=0>[N] event_time; //failure time
+  int s[K]; //group sizes
+}
+parameters{
+  real<lower=0> beta;
+  real<lower=0> theta;
+}
+model{
+  int position;
+  position = 1;
+  for (k in 1:K){
+    segment(event_time, position, s[k]) ~ nhpp(beta, theta, tau[k]);
+    position = position + s[k];
+  }
+//PRIORS
+  beta ~ gamma(1, 1);
+  theta ~ gamma(1, 0.01);
+}
+'
+
+plpn = '
+functions{
+  real nhpp_log(vector t, real beta, real theta, real tn){
+    vector[num_elements(t)] loglik_part;
+    real loglikelihood;
+    for (i in 1:num_elements(t)){
+      loglik_part[i] = log(beta) - beta*log(theta) + (beta - 1)*log(t[i]);
+    }
+    loglikelihood = sum(loglik_part) - (tn/theta)^beta;
+    return loglikelihood;
+  }
+}
+data {
+  int<lower=0> N; //total # of obs
+  int<lower=0> K; //total # of groups
+  vector<lower=0>[K] tn;//truncated time
+  vector<lower=0>[N] event_time; //failure time
+  int s[K]; //group sizes
+}
+parameters{
+  real<lower=0> beta;
+  real<lower=0> theta;
+}
+model{
+  int position;
+  position = 1;
+  for (k in 1:K){
+    segment(event_time, position, s[k]) ~ nhpp(beta, theta, tn[k]);
+    position = position + s[k];
+  }
+//PRIORS
+  beta ~ gamma(1, 1);
+  theta ~ gamma(1, 0.01);
+}
+'
+
